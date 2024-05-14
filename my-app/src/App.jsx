a532346ae5d95447be3ba5b6e7c5a95758e901ab
Hasnain Ali <93475112+HasnainAli313@users.noreply.github.com>
2024-05-14 18:58:12 +0500

    react-app

---
+++ my-app/src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+        <header>
+          <h1>Hello World</h1>
+        </header>
+    </>
+  )
+}
+
+export default App
